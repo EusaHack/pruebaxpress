24afc27ab77568dd8c0a38f18737dc7ccaaa5aec
EusaHack <eusa510@gmail.com>
2025-09-04 22:37:51 -0600

    [FEATURE] qa

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 // Token que aceptaremos (puedes ponerlo en variables de entorno)
 const TOKEN = process.env.TOKEN;
 const key = process.env.AES_KEY;
+const key_qa = process.env.AES_KEY_QA;
 
 // Middleware para validar token
 function validarToken(req, res, next) {
@@ -52,6 +53,41 @@ app.post("/cifrar",validarToken, async (req, res) => {
 
     // 🚀 Enviar al endpoint real
     const response = await axios.post("https://sandboxpo.mit.com.mx/gen", data, {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    });
+
+    // Respuesta para el frontend
+    res.json({
+      success: true,
+      ciphertext,
+      data,
+      mitResponse: response.data,
+    });
+
+  } catch (error) {
+    console.error("Error en /cifrar:", error.message);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+
+app.post("/cifrar-qa",validarToken, async (req, res) => {
+  try {
+    const { xml } = req.body; // <-- XML enviado desde el front
+
+    if (!xml) {
+      return res.status(400).json({ success: false, error: "Falta el campo xml" });
+    }
+
+    // 🔐 Encriptar XML recibido
+    const ciphertext = CryptoJS.AES.encrypt(xml, key_qa).toString();
+
+    // Construir data
+    const originalString1 = `xml=<pgs><data0>9265660202</data0><data>${ciphertext}</data></pgs>`;
+    const data = encodeURIComponent(originalString1);
+
+    // 🚀 Enviar al endpoint real
+    const response = await axios.post("https://qa5.mitec.com.mx/p/gen", data, {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
     });
 
